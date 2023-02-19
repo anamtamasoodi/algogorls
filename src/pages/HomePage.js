8e299339d094bf1f5964e7133a30a0d55ca3b841
anamta03 <45716679+anamtamasoodi@users.noreply.github.com>
2023-02-19 04:16:26 -0500

    Added more to form, did some formatting

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,142 +4,142 @@ import Header from "./Header";
 
 const HomePage = ({}) => {
 
-// functions
-const GenerateNowButton = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const SkyBlueRectangle1Function = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const SkyBlueRectangleFunction = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const WhiteRectangleFunction = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const WhiteRectangle2Function = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const EmailButtonFunction = (e, name) => {
-   alert(`${name} was clicked`);
-};
-const EmailButton2Function = (e, name) => {
-   alert(`${name} was clicked`);
-};
+   // functions
+   const GenerateNowButton = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const SkyBlueRectangle1Function = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const SkyBlueRectangleFunction = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const WhiteRectangleFunction = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const WhiteRectangle2Function = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const EmailButtonFunction = (e, name) => {
+      alert(`${name} was clicked`);
+   };
+   const EmailButton2Function = (e, name) => {
+      alert(`${name} was clicked`);
+   };
 
 
-const HomePage = styled.div`
-position: relative;
-gap: 103px;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-padding: 0px 0px 175px 0px;
-box-sizing: border-box;
-background-color: #f3f3f3;
-overflow: hidden;
-`;
+   const HomePage = styled.div`
+   position: relative;
+   gap: 103px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   padding: 0px 0px 175px 0px;
+   box-sizing: border-box;
+   background-color: #f3f3f3;
+   overflow: hidden;
+   `;
 
-return (
-   <HomePage>
-      <Header>
-      </Header>
-      <Text8>
-      Made by Team AlgoGorls at ElleHacks
-      <Paragraph6>
+   return (
+      <HomePage>
+         <Header>
+         </Header>
+         <Text8>
+         Made by Team AlgoGorls at ElleHacks
+         <Paragraph6>
+            <br />
+            UI/UX Designer: Kelly Owenya
+            <br />
+            Front-End Developer: Anamta Masoodi
+            <br />
+            Back-End Developer: Cecelia Nydam
+         </Paragraph6>
+         </Text8>
+         <Paragraph5>
+         Environmentalists?
          <br />
-         UI/UX Designer: Kelly Owenya
+         Meet artificial <br />
+         intelligence.
+         </Paragraph5>
+         <Text3>
+         Fight for the issues
          <br />
-         Front-End Developer: Anamta Masoodi
-         <br />
-         Back-End Developer: Cecelia Nydam
-      </Paragraph6>
-      </Text8>
-      <Paragraph5>
-      Environmentalists?
-      <br />
-      Meet artificial <br />
-      intelligence.
-      </Paragraph5>
-      <Text3>
-      Fight for the issues
-      <br />
-      you care about.
-      </Text3>
-      <Group>
-      <Group2>
-         <Paragraph4>
-            Effortlessly advocate for the earth with EcoEcho.
-         </Paragraph4>
-         <Group3>
-            <TropicalRainForestRectangle
-            onClick={(e) =>
-               GenerateNowButton(
-                  e,
-                  "TropicalRainForestRectangle"
-               )
-            }
-            >
-            <Text5>Generate an email</Text5>
-            </TropicalRainForestRectangle>
-            <SkyBlueRectangle1
-            onClick={(e) => SkyBlueRectangle1Function(e, "SkyBlueRectangle1")}
-            >
-            <Text4>Make a petition</Text4>
-            </SkyBlueRectangle1>
-         </Group3>
-      </Group2>
-      <SkyBlueRectangle
-         onClick={(e) => SkyBlueRectangleFunction(e, "SkyBlueRectangle")}
-      >
-         <Text2>Find your MP</Text2>
-      </SkyBlueRectangle>
-      <Paragraph1>
-         Advocacy made more accessible than ever — make a change with just a
-         few clicks!
-      </Paragraph1>
-      <Paragraph>
-         Amplify your voice and get in touch with lawmakers.
-      </Paragraph>
-      <PoliticianGraphic>
-         <WhiteRectangle
-            onClick={(e) => WhiteRectangleFunction(e, "WhiteRectangle")}
+         you care about.
+         </Text3>
+         <Group>
+         <Group2>
+            <Paragraph4>
+               Effortlessly advocate for the earth with EcoEcho.
+            </Paragraph4>
+            <Group3>
+               <TropicalRainForestRectangle
+               onClick={(e) =>
+                  GenerateNowButton(
+                     e,
+                     "TropicalRainForestRectangle"
+                  )
+               }
+               >
+               <Text5>Generate an email</Text5>
+               </TropicalRainForestRectangle>
+               <SkyBlueRectangle1
+               onClick={(e) => SkyBlueRectangle1Function(e, "SkyBlueRectangle1")}
+               >
+               <Text4>Make a petition</Text4>
+               </SkyBlueRectangle1>
+            </Group3>
+         </Group2>
+         <SkyBlueRectangle
+            onClick={(e) => SkyBlueRectangleFunction(e, "SkyBlueRectangle")}
          >
-            <EnterYourLocation1>
-            |<EnterYourLocation>Enter your location...</EnterYourLocation>
-            </EnterYourLocation1>
-         </WhiteRectangle>
-      </PoliticianGraphic>
-      <Element1>
-         <Image1 src={`undefined`} />
-      </Element1>
-      </Group>
-      <Group1>
-      <Logo src={`https://file.rendit.io/n/vtmrLJRAlj5fZtwXpbxK.png`} />
-      <Paragraph3>
-         At EcoEcho, we believe that the health of our planet is one of the
-         most important issues we face today. The Earth is our home, and we are
-         committed to doing everything we can to protect it. That's why we
-         created EcoEcho - to make it easier for people like you to speak up
-         for the planet and advocate for change.
-      </Paragraph3>
-      <WhiteRectangle2
-         onClick={(e) => WhiteRectangle2Function(e, "WhiteRectangle2")}
-      >
-         <EnterAGlobalIssue1>
-            |<EnterAGlobalIssue>Enter a global issue...</EnterAGlobalIssue>
-         </EnterAGlobalIssue1>
-         <Image3 src={`https://file.rendit.io/n/GRAhamvsgtdqWMOOOj4R.png`} />
-      </WhiteRectangle2>
-      <Image2 src={`https://file.rendit.io/n/nlYO09DU9uViq2Qhgf3p.svg`} />
-      <OliveGreenRectangle
-         onClick={(e) => GenerateNowButton(e, "OliveGreenRectangle")}
-      >
-         <Text1>Generate Now</Text1>
-      </OliveGreenRectangle>
-      </Group1>
-   </HomePage>
-);
+            <Text2>Find your MP</Text2>
+         </SkyBlueRectangle>
+         <Paragraph1>
+            Advocacy made more accessible than ever — make a change with just a
+            few clicks!
+         </Paragraph1>
+         <Paragraph>
+            Amplify your voice and get in touch with lawmakers.
+         </Paragraph>
+         <PoliticianGraphic>
+            <WhiteRectangle
+               onClick={(e) => WhiteRectangleFunction(e, "WhiteRectangle")}
+            >
+               <EnterYourLocation1>
+               |<EnterYourLocation>Enter your location...</EnterYourLocation>
+               </EnterYourLocation1>
+            </WhiteRectangle>
+         </PoliticianGraphic>
+         <Element1>
+            <Image1 src={`undefined`} />
+         </Element1>
+         </Group>
+         <Group1>
+         <Logo src={`https://file.rendit.io/n/vtmrLJRAlj5fZtwXpbxK.png`} />
+         <Paragraph3>
+            At EcoEcho, we believe that the health of our planet is one of the
+            most important issues we face today. The Earth is our home, and we are
+            committed to doing everything we can to protect it. That's why we
+            created EcoEcho - to make it easier for people like you to speak up
+            for the planet and advocate for change.
+         </Paragraph3>
+         <WhiteRectangle2
+            onClick={(e) => WhiteRectangle2Function(e, "WhiteRectangle2")}
+         >
+            <EnterAGlobalIssue1>
+               |<EnterAGlobalIssue>Enter a global issue...</EnterAGlobalIssue>
+            </EnterAGlobalIssue1>
+            <Image3 src={`https://file.rendit.io/n/GRAhamvsgtdqWMOOOj4R.png`} />
+         </WhiteRectangle2>
+         <Image2 src={`https://file.rendit.io/n/nlYO09DU9uViq2Qhgf3p.svg`} />
+         <OliveGreenRectangle
+            onClick={(e) => GenerateNowButton(e, "OliveGreenRectangle")}
+         >
+            <Text1>Generate Now</Text1>
+         </OliveGreenRectangle>
+         </Group1>
+      </HomePage>
+   );
 };
 
 
